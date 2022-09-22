df5b0a183cb6312efb1fcc78a80d8411ee554039
mesut <mesuttgedik@gmail.com>
2022-09-21 21:54:48 -0700

    Projects back up

---
+++ Archive/trying.playground/Contents.swift
@@ -0,0 +1,20 @@
+func exercise() {
+    
+    //The number of letters in alphabet equals 26
+    let alphabet = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
+    
+    // 🚨 Don't change the code above 👆
+    //####################################
+    //Write your code below this line 👇
+    let password = alphabet[Int.random(in: 0 ... 25)]
+
+
+
+    //Write your code above this line 👆
+    //####################################
+
+    //🚨 Don't change the code below 👇
+    print(password)
+
+    
+}
